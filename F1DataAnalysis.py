da0ac5a323a17c0cfaef8ca8cd290467343ee417
Josh Maddison <joshmaddison4@gmail.com>
2025-12-05 15:23:21 +0000

    Add files via upload

--- F1DataAnalysis.py
+++ F1DataAnalysis.py
@@ -19,20 +19,33 @@ tel1 = lap1.get_telemetry()
 tel2 = lap2.get_telemetry()
 
 #Plotting
-plt.figure(figsize=(12, 5))
+#plt.figure(figsize=(12, 5))
 #plt.plot(tel1['Distance'], tel1['Speed'], label='VER Speed')
 #plt.plot(tel2['Distance'], tel2['Speed'], label='HAM Speed')
 
 #Adding Throttle and Brake Traces
 #plt.plot(tel1['Distance'], tel1['Throttle'], label='VER Throttle')
-plt.plot(tel1['Distance'], tel1['Brake'], label='VER Brake')
+#plt.plot(tel1['Distance'], tel1['Brake'], label='VER Brake')
 #plt.plot(tel2['Distance'], tel2['Throttle'], label='HAM Throttle')
-plt.plot(tel2['Distance'], tel2['Brake'], label='HAM Brake')
-    
-plt.title("Brake Trace Comparison - Ham vs VER")
-plt.xlabel("Distance (m)")
-plt.ylabel("Brake Position (%)")
-plt.legend()
-plt.grid(True)
+#plt.plot(tel2['Distance'], tel2['Brake'], label='HAM Brake')
+
+
+#Adding Gear Heatmap
+pos = tel2[['X', 'Y']]
+gear = tel2['nGear'].astype(int)
+
+plt.figure(figsize=(8,8))
+plt.scatter(pos['X'], pos['Y'], c=gear, cmap='plasma', s=5)
+
+plt.gca().set_aspect('equal')
+plt.title("Gear Usage Heatmap - HAM")
+plt.colorbar(label="Gear")
+            
+
+#plt.title("Brake Trace Comparison - Ham vs VER")
+#plt.xlabel("Distance (m)")
+#plt.ylabel("Brake Position (%)")
+#plt.legend()
+#plt.grid(True)
 plt.show()
 
